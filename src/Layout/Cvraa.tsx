b6fa398ebdb4c5b37f8e8f6d8b4628b366dd7c48
Jgarette0 <aajiliangarette@gmail.com>
2024-05-09 20:02:01 +0800

    :Congratulations page, CVRAA player

--- src/Layout/Cvraa.tsx
+++ src/Layout/Cvraa.tsx
@@ -1,6 +1,5 @@
 import { X } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 function Cvraa() {
   const [isVisible, setIsVisible] = useState(true);
@@ -13,9 +12,9 @@ function Cvraa() {
       {isVisible && (
         <div className="group fixed bottom-4 left-4 w-[300px] rounded-md hover:bg-primary-foreground transition-all duration-400 ease-in-out cursor-pointer bg-primary">
           <div className="relative">
-            <Link to="/congratulation">
+            <a href="/congratulation">
               <img src="img/cvraa.png" alt="" className="object-cover" />
-            </Link>
+            </a>
             <div
               className="group-hover:text-primary hover:bg-secondary rounded-full p-1 text-secondary absolute top-2 right-2"
               onClick={hideImg}
