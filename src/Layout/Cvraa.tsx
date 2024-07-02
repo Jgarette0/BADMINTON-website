9787aa76639d152068d2e387234895fb92e31149
Jgarette0 <aajiliangarette@gmail.com>
2024-07-02 20:19:40 +0800

    remove pop up

--- src/Layout/Cvraa.tsx
+++ src/Layout/Cvraa.tsx
@@ -1,15 +1,15 @@
-import { X } from "lucide-react";
-import { useState } from "react";
+// import { X } from "lucide-react";
+// import { useState } from "react";
 
 function Cvraa() {
-  const [isVisible, setIsVisible] = useState(true);
+  // const [isVisible, setIsVisible] = useState(true);
 
-  const hideImg = () => {
-    setIsVisible(true);
-  };
+  // const hideImg = () => {
+  //   setIsVisible(true);
+  // };
   return (
     <>
-      {isVisible && ( 
+      {/* {isVisible && ( 
         <div className="group fixed top-4 left-4 w-[300px] rounded-md hover:bg-primary-foreground transition-all duration-400 ease-in-out cursor-pointer bg-primary">
           <div className="relative">
             <a href="/congratulation">
@@ -23,7 +23,7 @@ function Cvraa() {
             </div>
           </div>
         </div>
-      )}
+      )} */}
     </>
   );
 }
